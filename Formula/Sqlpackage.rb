d2173fce4afde64e0b7e8ef4a9ff6b88c6fbc12a
Fredrik Larsson <nossralf@gmail.com>
2019-10-29 20:31:33 +0100

    Version 18.4

--- Formula/Sqlpackage.rb
+++ Formula/Sqlpackage.rb
@@ -1,9 +1,9 @@
 class Sqlpackage < Formula
   desc "SqlPackage is a command-line utility that automates various MSSQL tasks"
   homepage "https://docs.microsoft.com/en-us/sql/tools/sqlpackage-download"
-  url "https://go.microsoft.com/fwlink/?linkid=2102894"
-  sha256 "bf93983fd3a121a628ce0879584d8952ebccb5401459314fe348f5e163f40f13"
-  version "18.3.1"
+  url "https://go.microsoft.com/fwlink/?linkid=2108815"
+  sha256 "c7e5f1f5fa2342a73286e66aeb9b6921296e33db9cdc06876d547148c8634da6"
+  version "18.4"
 
   bottle :unneeded
 
