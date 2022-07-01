51be695ed33871eb409a062dbcd5acf110078155
Fredrik Larsson <nossralf@gmail.com>
2022-07-01 15:16:16 +0200

    Version 19.1

--- Formula/Sqlpackage.rb
+++ Formula/Sqlpackage.rb
@@ -1,9 +1,9 @@
 class Sqlpackage < Formula
   desc "SqlPackage is a command-line utility that automates various MSSQL tasks"
   homepage "https://docs.microsoft.com/en-us/sql/tools/sqlpackage/sqlpackage"
-  url "https://go.microsoft.com/fwlink/?linkid=2185765"
-  sha256 "8e7c50f301e4833e52a4d611b22bd22fb98e4658cbdeab7fda2862488336731a"
-  version "19.0"
+  url "https://go.microsoft.com/fwlink/?linkid=2196439"
+  sha256 "78b0c042c67685b3a8c9531ae978b74482ebc6da4adade23f57f6ccb3675d580"
+  version "19.1"
 
   def install
     libexec.install Dir["*"]
