e69cbae97b4384e9efd3157c6950417a93755cb7
Fredrik Larsson <nossralf@gmail.com>
2022-11-15 18:06:35 +0100

    Version 161.6374.0
    Also indicate that only x86-64 is supported.

--- Formula/Sqlpackage.rb
+++ Formula/Sqlpackage.rb
@@ -1,9 +1,11 @@
 class Sqlpackage < Formula
   desc "SqlPackage is a command-line utility that automates various MSSQL tasks"
   homepage "https://docs.microsoft.com/en-us/sql/tools/sqlpackage/sqlpackage"
-  url "https://go.microsoft.com/fwlink/?linkid=2209610"
-  sha256 "59940e91c56df616576b3b420bb0610d82c6ce2a82a02dcb3bacc50dd5bf4d23"
-  version "19.2"
+  url "https://go.microsoft.com/fwlink/?linkid=2215401"
+  sha256 "47b9a72a6c3d732e881d77678cc9af7e2a27a9f7dd47e160ed03afb74304d50c"
+  version "161.6374.0"
+
+  depends_on arch: :x86_64
 
   def install
     libexec.install Dir["*"]
